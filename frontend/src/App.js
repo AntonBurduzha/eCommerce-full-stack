9596e61bc705f821d1a8c5c1a6363a62c40bda03
antonburduzha <anton.burduzha@hotmail.com>
2020-11-12 10:31:57 +0200

    added product pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { Header, Footer } from 'components';
+import { HomeScreen, ProductScreen } from 'screens';
+
 const App = () => {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+    return (
+        <Router>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <Route path="/" component={HomeScreen} exact />
+                    <Route path="/product/:id" component={ProductScreen} />
+                </Container>
+            </main>
+            <Footer />
+        </Router>
+    );
+};
 
 export default App;
