d78fc3e87844fda43c8ccddc090f9c4d65e4f812
antonburduzha <anton.burduzha@hotmail.com>
2020-11-26 15:13:52 +0200

    added search and pagination for the products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import {
     deleteProduct,
     createProduct,
     updateProduct,
+    createProductReview,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
@@ -12,5 +13,6 @@ const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
